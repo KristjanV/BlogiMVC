41d03150f06aec27b8975bcbf6b5d363266949c4
Kristjan Vall <kristjan.vallimae@gmail.com>
2013-06-17 11:34:50 +0300

    Blogi mootor

---
+++ MvcApplication18/Controllers/AdminController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication18.Models;
+
+namespace MvcApplication18.Controllers
+{ 
+    public class AdminController : Controller
+    {
+        private BlogDBContext db = new BlogDBContext();
+
+        //
+        // GET: /Admin/
+
+        public ViewResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        //
+        // GET: /Admin/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+        //
+        // GET: /Admin/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Admin/Create
+
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(blog);
+        }
+        
+        //
+        // GET: /Admin/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+        //
+        // POST: /Admin/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+        //
+        // GET: /Admin/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            return View(blog);
+        }
+
+        //
+        // POST: /Admin/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Blog blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication18/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication18.Models
+{
+    public class Blog
+    {
+        public int id { get; set; }
+        public string pealkiri { get; set; }
+        public DateTime kuupaev { get; set; }
+        public string sisu { get; set; }
+    }
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
